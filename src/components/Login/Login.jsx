fc18a79e8486274380798a1cb24ad83663ae7c4b
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-14 22:26:04 +0200

    changes css

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -20,34 +20,35 @@ export const Login = () => {
   return (
     <div>
       <div
-        className="hero min-h-screen bg-cover bg-center"
+        className="hero min-h-screen bg-cover bg-center relative overflow-hidden"
         style={{
           backgroundImage:
             "url(https://images.cdn.kodinkuvalehti.fi/99-yNH-OtbZYiemGMa0XGUFjgzU=/1230x0/smart/kodinkuvalehti.fi/s3fs-public/main_media/pizza_page_1_image_0001.jpg?itok=S-tOl-4E)",
         }}
       >
-        <div className="absolute inset-0 bg-black opacity-60"></div>
+        <div className="absolute inset-0 bg-black opacity-80"></div>
         <div className="hero-content grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="text-center lg:text-left text-white">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+          <div className="text-center lg:text-left text-neutral-300 tracking-widest animate-slide-up">
+            <h1 className="text-5xl text-orange-500 tracking-widest">
+              Join us!
+            </h1>
             <p className="py-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-              in cupiditate modi iste eligendi dolor atque voluptate corrupti?
-              Fugiat eveniet ex perferendis cumque, quibusdam dolorem dicta
-              dignissimos corporis modi magnam!Fugiat eveniet ex perferendis
-              cumque, quibusdam dolorem dicta dignissimos corporis modi magnam!
+              Welcome to our cozy pizzeria, where every slice tells a story of
+              tradition, passion, and flavor. Nestled in the heart of the city,
+              our little corner of Italy brings the warmth and aroma of freshly
+              baked pies to your table.
             </p>
           </div>
-          <div className="card max-w-sm shadow-4xl bg-white bg-opacity-40  ">
+          <div className="card max-w-sm shadow-4xl bg-white bg-opacity-40">
             <Formik initialValues={initialValues} onSubmit={handleSubmit}>
               {({ handleSubmit }) => (
                 <Form
                   onSubmit={handleSubmit}
-                  className="card-body grid grid-cols-1 gap-6 "
+                  className="card-body grid grid-cols-1 gap-6"
                 >
                   <div className="form-control">
                     <label className="label text-white">
-                      <span className="label-text">Email</span>
+                      <span className="label-text text-white">Email</span>
                     </label>
                     <Field
                       name="email"
@@ -59,7 +60,7 @@ export const Login = () => {
                   </div>
                   <div className="form-control">
                     <label className="label text-white">
-                      <span className="label-text">Password</span>
+                      <span className="label-text text-white">Password</span>
                     </label>
                     <Field
                       name="password"
@@ -72,8 +73,9 @@ export const Login = () => {
                   <div className="form-control">
                     <button
                       type="submit"
-                      className="btn btn-primary bg-orange-600 hover:bg-orange-900 text-white rounded-full border-none"
+                      className="btn btn-primary bg-orange-600 text-white rounded-full border-none overflow-hidden relative"
                     >
+                      <span className="bg-gradient-to-r from-orange-500 to-orange-700 "></span>
                       Login
                     </button>
                   </div>
