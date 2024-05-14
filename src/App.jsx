3b33e646c907fb0747957564d2fd7a63ab46a38a
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-14 14:57:12 +0200

    added formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Login } from "./components/Login/Login"
+
 export const App = () => {
   return (
     <div>
-      <h2 className="text-5xl text-center">REACT PIZZA</h2>
+    <Login />
     </div>
   )
 }
