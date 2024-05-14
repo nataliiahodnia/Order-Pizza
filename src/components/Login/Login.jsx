067bdf13790666fdac23d17ed2046cdb497778cf
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-14 20:43:31 +0200

    change hero css

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -19,9 +19,16 @@ export const Login = () => {
 
   return (
     <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
+      <div
+        className="hero min-h-screen bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url(https://images.cdn.kodinkuvalehti.fi/99-yNH-OtbZYiemGMa0XGUFjgzU=/1230x0/smart/kodinkuvalehti.fi/s3fs-public/main_media/pizza_page_1_image_0001.jpg?itok=S-tOl-4E)",
+        }}
+      >
+        <div className="absolute inset-0 bg-black opacity-60"></div>
+        <div className="hero-content grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="text-center lg:text-left text-white">
             <h1 className="text-5xl font-bold">Login now!</h1>
             <p className="py-6">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
@@ -31,40 +38,46 @@ export const Login = () => {
               cumque, quibusdam dolorem dicta dignissimos corporis modi magnam!
             </p>
           </div>
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-              <Form className="card-body">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <Field
-                    name="email"
-                    type="email"
-                    placeholder="email"
-                    className="input input-bordered"
-                    required
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <Field
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                    className="input input-bordered"
-                    required
-                  />
-                </div>
-                <div className="form-control mt-6">
-                  <button type="submit" className="btn btn-primary">
-                    Login
-                  </button>
-                </div>
-              </Form>
-            </Formik>
+          <div className="card max-w-sm shadow-2xl bg-white bg-opacity-40">
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+  {({ handleSubmit }) => (
+    <form onSubmit={handleSubmit} className="card-body grid grid-cols-1 gap-6">
+      <div className="form-control">
+        <label className="label text-white">
+          <span className="label-text">Email</span>
+        </label>
+        <Field
+          name="email"
+          type="email"
+          placeholder="email"
+          className="input input-bordered text-black"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <label className="label text-white">
+          <span className="label-text">Password</span>
+        </label>
+        <Field
+          name="password"
+          type="password"
+          placeholder="password"
+          className="input input-bordered text-black"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <button
+          type="submit"
+          className="btn btn-primary bg-yellow-600 hover:bg-yellow-700 text-white"
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  )}
+</Formik>
+
           </div>
         </div>
       </div>
