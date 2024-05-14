2d6fe5fbfdd9f89b383351e0339d88ca8a5a08f8
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-14 13:57:25 +0200

    apdate localhost

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { PizzaProvider } from './store/PizzaProvider.jsx'
-import { PageProvider } from './store/PizzaProvider.jsx' 
-import { AuthProvider } from './store/AuthProvider.jsx'
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+import "./index.css";
+import { PizzaProvider } from "./store/PizzaProvider.jsx";
+import { PageProvider } from "./store/PageProvider.jsx";
+import { AuthProvider } from "./store/AuthProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<PizzaProvider>
-  <PageProvider>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </PageProvider>
-</PizzaProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PizzaProvider>
+    <PageProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </PageProvider>
+  </PizzaProvider>
+);
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	build: {
+		sourcemap: true,
+	},
+	preview: {
+		port: 7070,
+		strictPort: true,
+	},
+	server: {
+		port: 7070,
+		strictPort: true,
+		host: true,
+		origin: 'http://0.0.0.0:7070',
+	},
 })
