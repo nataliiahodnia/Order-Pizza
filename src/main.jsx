4d72b05fdcf358dfead2f636fa1dc491ccade311
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-16 15:43:20 +0200

    adde files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
-import "./index.css";
+import "./css/index.css";
 // import { PizzaProvider } from "./store/PizzaProvider.jsx";
 import { PageProvider } from "./store/PageProvider.jsx";
 import { AuthProvider } from "./store/AuthProvider.jsx";
