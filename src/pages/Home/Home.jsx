e87179b2dfda0678e7165c4102388e2af9ebc73f
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-16 17:12:34 +0200

    new test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,13 +14,15 @@ export const Home = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className={css.mainTextHome}>Hello, admin!</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Thank you for managing our system efficiently. Your dedication is
+            greatly appreciated.
           </p>
-          <button onClick={() => setPage("pizzas")} className={css.btnGetStarted}>
+          <button
+            onClick={() => setPage("pizzas")}
+            className={css.btnGetStarted}
+          >
             Get Started
           </button>
         </div>
