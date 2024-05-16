6ab7ca74d3329f085609dd1b2cb5de6df5508c68
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-16 20:22:03 +0200

    change bg

--- src/pages/Pizzas.jsx
+++ src/pages/Pizzas.jsx
@@ -15,11 +15,11 @@ export const Pizzas = () => {
   return (
     <div
       className="relative min-h-screen"
-      style={{
-        backgroundImage:
-          "url(https://s23209.pcdn.co/wp-content/uploads/2022/05/220310_DD_White-Pizza_312-760x1140.jpg) ",
-        backgroundSize: "cover",
-      }}
+      // style={{
+      //   backgroundImage:
+      //     "url(https://s23209.pcdn.co/wp-content/uploads/2022/05/220310_DD_White-Pizza_312-760x1140.jpg) ",
+      //   backgroundSize: "cover",
+      // }}
     >
       <div className="absolute inset-0 z-[1] bg-black bg-opacity-50"></div>
       <div className="relative z-[2] ">
